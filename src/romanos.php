3e8cfb523b350cf9d0d5831e2d3716f44aa93a44
Aldana Zarate <31429603+alduzarate@users.noreply.github.com>
2017-09-11 23:12:38 -0300

    Update romanos.php

--- src/romanos.php
+++ src/romanos.php
@@ -1,51 +1,61 @@
 <?php
-public class romanos {
+abstract public class romanos {
 
 }
 public class decimales {
-    public function aRomano(decimales n){
+	public $n;
+	
+	function __construct($numero)
+	{
+		$this->n=$numero;
+	}
+    public function aRomano(){
 
-       if (n>=1000){
-            return "M" . aRomano(n-1000); 
-       elseif(n>=900){
-            return "CM" . aRomano (n-900);
+       if ($this->n >= 1000){
+            return "M" . aRomano($this->n - 1000); 
+       elseif($this->n >=900){
+            return "CM" . aRomano ($this->n - 900);
         }
-       elseif (n>=500){
-            return "D" . aRomano(n-500);
+       elseif ($this->n >=500){
+            return "D" . aRomano($this->n - 500);
     }
 
-        elseif (n>=400){
-            return "CD" . aRomano (n-400);
+        elseif ($this->n >= 400){
+            return "CD" . aRomano ($this->n - 400);
     }
 
-        elseif (n>=100){
-            return "C" . aRomano(n-100);
+        elseif ($this->n >= 100){
+            return "C" . aRomano($this->n - 100);
     }
-        elseif (n>=90){
-            return "XC" . aRomano (n-90);
+        elseif ($this->n >= 90){
+            return "XC" . aRomano ($this->n - 90);
     }
-        elseif (n>=50){
-            return "L" . aRomano (n-50);
+        elseif ($this->n >= 50){
+            return "L" . aRomano ($this->n - 50);
     }
-        elseif (n>=40){
-            return "XL" . aRomano (n-40);
+        elseif ($this->n >= 40){
+            return "XL" . aRomano ($this->n - 40);
     }
-        elseif (n>=10){
-            return "X" . aRomano (n-10);
+        elseif ($this->n >= 10){
+            return "X" . aRomano ($this->n - 10);
     }
-        elseif (n==9){
+        elseif ($this->n == 9){
             return "IX";
     }
-        elseif (n>=5){
-            return "V" . aRomano(n-5);
+        elseif ($this->n >= 5){
+            return "V" . aRomano($this->n - 5);
     }
-        elseif (n==4){
+        elseif ($this->n == 4){
             return "IV";
     }
-        elseif (n<4 && n!=0){
-            return "I" . aRomano (n-1);
+        elseif ($this->n <4 && $this->n != 0){
+            return "I" . aRomano ($this->n - 1);
     }
         else return;
 }
+	}
+}
+$cinco= new decimales(5);
+print $cinco->aRomano();
 
 ?>
